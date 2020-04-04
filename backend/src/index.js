a37ead1bfb0ae8e312c37197698722230b16a080
Dev-Nando <sesisnando.neto@apoemafabsoft.com.br>
2020-04-04 13:39:44 -0400

    Backend, Web e Mobile

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket')
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect('mongodb://root:root@cluster0-shard-00-00-anapj.mongodb.net:27017,cluster0-shard-00-01-anapj.mongodb.net:27017,cluster0-shard-00-02-anapj.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
